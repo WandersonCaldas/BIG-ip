7f91d5806448eb18be4cec6aec5a46ed57d04371
WandersonCaldas <wcaldasti@gmail.com>
2018-07-21 00:06:19 -0300

    CRIAR SOFTWARE
    Signed-off-by: WandersonCaldas <wcaldasti@gmail.com>

---
+++ BIG-IP/src/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Gerar = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(22, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Origem"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(95, 10)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(425, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Gerar
+        '
+        Me.Gerar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Gerar.Location = New System.Drawing.Point(148, 105)
+        Me.Gerar.Name = "Gerar"
+        Me.Gerar.Size = New System.Drawing.Size(87, 23)
+        Me.Gerar.TabIndex = 2
+        Me.Gerar.Text = "Gerar"
+        Me.Gerar.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(317, 105)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(87, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Fechar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Destino"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(95, 68)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(425, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(541, 162)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Gerar)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "BIP-IP"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Gerar As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+
+End Class
---
+++ BIG-IP/src/Form1.vb
@@ -0,0 +1,87 @@
+﻿Imports Microsoft.VisualBasic.FileIO
+Imports System.Text
+Imports System.IO
+
+Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Gerar_Click(sender As Object, e As EventArgs) Handles Gerar.Click
+        Dim txtArquivoCSV As String = TextBox1.Text
+        Dim txtArquivoDestino As String = TextBox2.Text
+
+        'verifica se o nome do arquivo foi informado
+        Dim caminho As String = ""
+        If Not String.IsNullOrEmpty(txtArquivoDestino) Then
+            caminho = txtArquivoDestino
+        Else
+            MessageBox.Show("Informe o nome do arquivo texto ", "Nome do arquivo", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            TextBox2.Focus()
+            Return
+        End If
+
+        Using leitorArquivoCSV As New TextFieldParser(txtArquivoCSV, Encoding.Default)
+            'define o tipo de arquivo como delimitado
+            leitorArquivoCSV.TextFieldType = FileIO.FieldType.Delimited
+
+            'define o delimitador usado no arquivo
+            leitorArquivoCSV.SetDelimiters(";")
+
+            'Define o caractere que indica que a linha é um comentário
+            'leitorArquivoCSV.CommentTokens = New String() {"#"}
+
+            'Informa que existe um campo que esta envolto em aspas duplas (")
+            'leitorArquivoCSV.HasFieldsEnclosedInQuotes = True
+
+            'define um array de string
+            Dim linhaAtual As String()
+
+            'pula a primeira linha do arquivo 
+            leitorArquivoCSV.ReadLine()
+
+            ' Cria um arquivo para escrita
+            Using sw As FileStream = File.Create(caminho)
+                
+            End Using
+
+            'inicia o laço para iterar no arquivo texto
+            While Not leitorArquivoCSV.EndOfData
+                'ler uma linha do arquivo
+                linhaAtual = leitorArquivoCSV.ReadFields()
+
+                'MessageBox.Show(linhaAtual(0) & ";" & linhaAtual(1) & ";" & linhaAtual(2) & ";" & linhaAtual(3) & ";" & linhaAtual(4) & ";" & linhaAtual(5) & ";" & linhaAtual(6))
+
+                'verifica se o arquivo existe
+                If File.Exists(caminho) Then
+                    Dim nd = linhaAtual(3).Split("/")
+                    Dim nd_exibir As String = nd(UBound(nd))
+                    Dim a_mib = linhaAtual(4).Split(".")
+                    Dim mib As String = a_mib(UBound(a_mib))
+
+                    Using sw As StreamWriter = New StreamWriter(caminho, True)
+                        sw.WriteLine()
+                        sw.WriteLine("define service{")
+                        sw.WriteLine("        use                             linux-rem-srv")
+                        sw.WriteLine("        host_name                       " & linhaAtual(1).Trim & "")
+                        sw.WriteLine("        contact_groups                  geope_produ,geope_infra_telecom,geope_infra_webapp_n1")
+                        sw.WriteLine("        service_description             Check Pool " & linhaAtual(2).Replace("/Common/", "").Trim & " Member " & nd_exibir.ToString.Trim & ":" & mib.Trim)
+                        sw.WriteLine("        notes                           Monitoramento de checagem de status do Pool Member " & nd_exibir.ToString.Trim & ":" & mib.Trim & " - " & linhaAtual(5).Trim)
+                        sw.WriteLine("        check_command                   check_pool_bigip!192.168.104.121!@cabalbrasil!" & linhaAtual(4).Trim)
+                        sw.WriteLine("}")
+                        sw.WriteLine()
+                    End Using
+                Else
+                    MessageBox.Show("Arquivo não encontrado ", "Nome do arquivo", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    Application.Exit()
+                End If
+            End While
+
+            MessageBox.Show("ROTINA ENCERRADA")
+            Application.Exit()
+        End Using
+    End Sub
+
+
+End Class
